8cd2e1a0d627367361b8ee1bf63c5a598e6d314e
yadakan <yada.kankanokkul@gmail.com>
2024-11-10 16:12:10 -0800

    week6 lab1

--- django_project1/bicycle_app/admin.py
+++ django_project1/bicycle_app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, Order, Order_Item
+#admin.site.register(Product)
+#admin.site.register(Order)
+#admin.site.register(Order_Item)
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+    list_display = ('id', 'product_name', 'price')
+
+admin.site.register(Product, ProductAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+    pass
+    list_display = ('id', 'amount', 'order_date_time')
+
+admin.site.register(Order, OrderAdmin)
+
+class Order_ItemAdmin(admin.ModelAdmin):
+    pass
+    list_display = ('id','product_id', 'quantity', 'amount')
+
+admin.site.register(Order_Item, Order_ItemAdmin)
